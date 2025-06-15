e77a66c7895941135f956b375e99adeb346632a9
Schmidt, Oscar <os.schmidt@campus.lmu.de>
2025-06-15 21:12:18 +0200

    Renamed registration to register and added Register and Login to routing

--- foto-social-react/psis-image-based-social-media-app/src/App.tsx
+++ foto-social-react/psis-image-based-social-media-app/src/App.tsx
@@ -3,7 +3,8 @@ import './App.css'
 import MainPage from "./MainPage/mainpage.tsx";
 import {createTheme, ThemeProvider} from "@mui/material";
 import BottomNavigationBar from "./MainPage/bottomNavigationBar.tsx";
-
+import Login from "./Authentification/Login.tsx"
+import Register from "./Authentification/Register.tsx"
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
@@ -16,6 +17,8 @@ const App = () => {
       <ThemeProvider theme={darkTheme}>
         <Routes>
             <Route path="/groups" element={<MainPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             {/* TODO: Hier dann die friends bzw. settings page auskommentieren */}
             {/*<Route path="/friends" element={<FriendsPage />} />*/}
             {/*<Route path="/settings" element={<SettingsPage />} />*/}
