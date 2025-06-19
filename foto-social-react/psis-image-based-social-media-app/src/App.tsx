76073fc87f39f32b64c9b558572bc73966198e53
nayungao <99612911+nayungao@users.noreply.github.com>
2025-06-19 20:56:39 +0200

    add chatpage

--- foto-social-react/psis-image-based-social-media-app/src/App.tsx
+++ foto-social-react/psis-image-based-social-media-app/src/App.tsx
@@ -1,11 +1,15 @@
 import {Route, Routes } from 'react-router-dom';
 import './App.css'
 import MainPage from "./MainPage/mainpage.tsx";
-import {createTheme, ThemeProvider} from "@mui/material";
+import {createTheme, ThemeProvider, CssBaseline} from "@mui/material";
 import BottomNavigationBar from "./MainPage/bottomNavigationBar.tsx";
 import Login from "./Authentification/Login.tsx"
 import Register from "./Authentification/Register.tsx"
+import ChatPage from "./ChatPage/ChatBefore.tsx";
+
+
 const darkTheme = createTheme({
+  
   palette: {
     mode: 'dark',
   },
@@ -15,15 +19,20 @@ const App = () => {
   return (
     <>
       <ThemeProvider theme={darkTheme}>
-        <Routes>
+      <CssBaseline />
+     <div className="App">
+      <ChatPage />
+     </div>
+
+        {/* <Routes>
             <Route path="/groups" element={<MainPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             {/* TODO: Hier dann die friends bzw. settings page auskommentieren */}
             {/*<Route path="/friends" element={<FriendsPage />} />*/}
             {/*<Route path="/settings" element={<SettingsPage />} />*/}
-        </Routes>
-        <BottomNavigationBar />
+        {/* </Routes>
+        // <BottomNavigationBar /> */} 
     </ThemeProvider>
     </>
   )
