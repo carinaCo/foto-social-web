f23e5c4ef061c3c0bdcb7c58cd69909c7d54b298
Carina Cocora <carina.cocora@yahoo.com>
2025-06-23 14:57:22 +0200

    test class

---
+++ foto-social-react/psis-image-based-social-media-app/src/Client/use_cases/GroupManagement/runGetGroup.js
@@ -0,0 +1,48 @@
+import { GetGroup } from './GetGroup.js';
+import { FirestoreCommunicationHelper } from '../../../utils/firestoreCommunicationHelper.js';
+import { HttpClient } from '../../../utils/httpClient.js';
+import { getFirestoreAccessToken } from '../../../utils/getFirestoreToken.js';
+import { GetUserData } from '../UserManagement/GetUserData.js';
+
+
+const testGroup = async () => {
+  try {
+    const projectId = 'foto-social-web';
+    const groupId = '06281c9a-36c3-4087-8e24-eee6515bf9ee';
+
+    const getGroup = new GetGroup({ projectId });
+
+    const result = await getGroup.execute({ groupId });
+
+    console.log('RESULT:', JSON.stringify(result, null, 2));
+  } catch (error) {
+    console.error('ERROR:', error);
+  }
+};
+
+const testGetUserData = async () => {
+    try {
+      const projectId = 'foto-social-web';
+      const userId = '2f37950b-e32b-45fd-bc31-499dcda3efd4';
+  
+      const getUserData = new GetUserData({ projectId });
+  
+      const result = await getUserData.execute({ userId });
+  
+      console.log('RESULT:', JSON.stringify(result, null, 2));
+    } catch (error) {
+      console.error('ERROR:', error);
+    }
+  };
+/*
+const test = async () => {
+    try {
+      let token =   await getFirestoreAccessToken();
+  
+      console.log('RESULT:', JSON.stringify(token));
+    } catch (error) {
+      console.error('ERROR:', error);
+    }
+  };
+*/
+testGroup();
