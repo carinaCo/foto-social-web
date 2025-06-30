e254f065bbe3bf3777cde3b5362d3ed2a11a959c
Carina Cocora <carina.cocora@yahoo.com>
2025-06-30 22:39:55 +0200

    changes

--- foto-social-react/psis-image-based-social-media-app/src/utils/firestoreCommunicationHelper.js
+++ foto-social-react/psis-image-based-social-media-app/src/utils/firestoreCommunicationHelper.js
@@ -1,49 +1,49 @@
-export class FirestoreCommunicationHelper {
+  export class FirestoreCommunicationHelper {
     constructor({ projectId, databaseId = '(default)' }) {
       this.projectId = projectId;
       this.databaseId = databaseId;
     }
-  
+
     get baseFirestoreUrl() {
       return `https://firestore.googleapis.com/v1/projects/${this.projectId}/databases/${this.databaseId}/documents`;
     }
-  
+
     getUserDoc(userId) {
       return `${this.baseFirestoreUrl}/Users/${userId}`;
     }
-  
+
     registerUserUrl() {
       return `${this.baseFirestoreUrl}/Users`;
     }
-  
+
     getFriendsUrl(userId) {
-      return `${this.baseFirestoreUrl}/Users/${userId}/friends`;
+      return `${this.baseFirestoreUrl}/Users/${userId}/Friends`;
     }
-  
+
     getPendingFriendRequestsUrl(userId) {
-      return `${this.baseFirestoreUrl}/Users/${userId}/pendingFriendRequests`;
+      return `${this.baseFirestoreUrl}/Users/${userId}/PendingFriendRequests`;
     }
-  
+
     getBlockedByUserListUrl(userId) {
-      return `${this.baseFirestoreUrl}/Users/${userId}/blockedUsers`;
+      return `${this.baseFirestoreUrl}/Users/${userId}/BlockedUsers`;
     }
-  
+
     removeFriendUrl(userId, friendId) {
-      return `${this.baseFirestoreUrl}/Users/${userId}/friends/${friendId}`;
+      return `${this.baseFirestoreUrl}/Users/${userId}/Friends/${friendId}`;
     }
-  
+
     blockUserUrl(userId, targetUserId) {
-      return `${this.baseFirestoreUrl}/Users/${userId}/blockedUsers/${targetUserId}`;
+      return `${this.baseFirestoreUrl}/Users/${userId}/BlockedUsers/${targetUserId}`;
     }
-  
+
     sendFriendRequestUrl(senderId) {
-      return `${this.baseFirestoreUrl}/Users/${senderId}/sentFriendRequests`;
+      return `${this.baseFirestoreUrl}/Users/${senderId}/SentFriendRequests`;
     }
-  
+
     findMatchingUsersUrl() {
-      return `${this.baseFirestoreUrl}/Users:runQuery`;
+      return `${this.baseFirestoreUrl}:runQuery`;
     }
-  
+
     getGroupUrl(groupId) {
       return `${this.baseFirestoreUrl}/Groups/${groupId}`;
     }
@@ -51,23 +51,23 @@ export class FirestoreCommunicationHelper {
     getGroupUsersUrl(groupId) {
         return `${this.baseFirestoreUrl}/Groups/${groupId}/Users`;
       }
-  
+
     getGroupPostsUrl(groupId) {
-      return `${this.baseFirestoreUrl}/Groups/${groupId}/posts`;
+      return `${this.baseFirestoreUrl}/Groups/${groupId}/Posts`;
     }
-  
+
     getGroupMessagesUrl(groupId) {
-      return `${this.baseFirestoreUrl}/Groups/${groupId}/messages`;
+      return `${this.baseFirestoreUrl}/Groups/${groupId}/Messages`;
     }
-  
+
     getGroupFeedUrl(groupId) {
-      return `${this.baseFirestoreUrl}/Groups/${groupId}/feed`;
+      return `${this.baseFirestoreUrl}/Groups/${groupId}/Feed`;
     }
-  
+
     getGroupInvitesUrl(groupId) {
-      return `${this.baseFirestoreUrl}/Groups/${groupId}/invites`;
+      return `${this.baseFirestoreUrl}/Groups/${groupId}/Invites`;
     }
-  
+
     sendGroupInviteUrl(groupId) {
     }
 
@@ -80,17 +80,29 @@ export class FirestoreCommunicationHelper {
     }
 
     getGroupInviteUrl(groupId, inviteId) {
-      return `${this.baseFirestoreUrl}/Groups/${groupId}/invites/${inviteId}`;
+      return `${this.baseFirestoreUrl}/Groups/${groupId}/Invites/${inviteId}`;
     }
 
     sendGroupInviteUrl(groupId) {
-      return `${this.baseFirestoreUrl}/Groups/${groupId}/invites`;
+      return `${this.baseFirestoreUrl}/Groups/${groupId}/Invites`;
     }
     
     getUsersCollectionUrl() {
       return `${this.baseFirestoreUrl}/Users`;
     }    
-    
 
-}
+    getGroupsUrl() {
+      return `${this.baseFirestoreUrl}/Groups`;
+    }
+
+    getMessageRef(groupId, messageId) {
+      return `${this.baseFirestoreUrl}/Groups/${groupId}/Messages/${messageId}`;
+    }
+
+    getUsersQueryUrl() {
+      return `https://firestore.googleapis.com/v1/projects/${this.projectId}/databases/(default)/documents:runQuery`;
+    }
+    
+    
+  }
   
