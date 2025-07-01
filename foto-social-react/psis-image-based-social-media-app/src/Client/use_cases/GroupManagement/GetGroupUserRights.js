cb601a82e6e77b2c44efd6fdc5c89bc1cea13feb
Carina Cocora <carina.cocora@yahoo.com>
2025-07-01 19:24:53 +0200

    fixes due to test file

--- foto-social-react/psis-image-based-social-media-app/src/Client/use_cases/GroupManagement/GetGroupUserRights.js
+++ foto-social-react/psis-image-based-social-media-app/src/Client/use_cases/GroupManagement/GetGroupUserRights.js
@@ -1,2 +1,14 @@
 //TODO: Expects the group-ID and user-ID of a user
 //and retrieves the permissions within the group (tells a normal user from admin apart)
+
+import { FirestoreCommunicationHelper } from '../../../utils/firestoreCommunicationHelper.js';
+import { HttpClient } from '../../../utils/httpClient.js';
+import { getFirestoreAccessToken } from '../../../utils/getFirestoreAccessToken.js';
+
+export class GetGroupUserRights {
+  constructor({ projectId }) {
+    this.projectId = projectId;
+  }
+
+
+}
