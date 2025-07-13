f4c9b67e1177c390b28124dd5379dd59a4a1cbee
Andreas Vogl <andreasvogl64@gmail.com>
2025-07-13 15:55:43 +0200

    schonmal redirection wenn man auf logout clickt und logout button

--- foto-social-react/psis-image-based-social-media-app/src/SettingsPage/SettingsPage.tsx
+++ foto-social-react/psis-image-based-social-media-app/src/SettingsPage/SettingsPage.tsx
@@ -1,19 +1,35 @@
 import * as React from 'react';
 import {
-    AppBar, Box,
-    CssBaseline
+    AppBar, Box, Button,
+    CssBaseline,
+    Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle
 } from "@mui/material";
 import AppToolbar from "../GroupPage/AppToolbar.tsx";
 import SettingsIcon from "@mui/icons-material/Settings";
+import {useNavigate} from "react-router-dom";
 
 
 const SettingsPage: React.FC = () => {
+    const navigate = useNavigate();
+    const [open, setOpen] = React.useState(false);
+
+    const handleLogoutClick = () => {
+        setOpen(true);
+    };
+    const handleConfirmLogout = () => {
+        setOpen(false);
+        navigate('/login', { replace: true });
+    };
+
+    const handleCancel = () => {
+        setOpen(false);
+    };
 
     return (
         <>
             <CssBaseline enableColorScheme />
             <AppBar>
-                <AppToolbar />
+                <AppToolbar onAddClick={handleLogoutClick}/>
             </AppBar>
 
             <Box
@@ -35,6 +51,36 @@ const SettingsPage: React.FC = () => {
                     }}
                 />
             </Box>
+            <Dialog
+                open={open}
+                onClose={handleCancel}
+                slotProps={{
+                    paper: {
+                        sx: {
+                            bgcolor: 'rgba(36,17,86,0.2)',
+                            border: '1px solid rgba(255, 255, 255, 0.1)',
+                            borderRadius: '12px',
+                            backdropFilter: 'blur(6px)',
+                            color: '#fff'
+                        }
+                    }
+                }}
+            >
+                <DialogTitle>Logout bestätigen</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Möchtest du dich wirklich ausloggen?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCancel} color="primary">
+                        Abbrechen
+                    </Button>
+                    <Button onClick={handleConfirmLogout} color="error" variant="contained">
+                        Logout
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 }
